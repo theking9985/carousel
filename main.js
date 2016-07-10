de7171166ece82632ae1290cd81776a1a20274a2
theking9985 <theking9985@yahoo.com>
2016-07-09 18:27:51 -0700

    temporary working

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
 
     $("#right_arrow").click(function(){
         var leftIndent = parseInt($("ul").css("left")) - $("li").width();
-        
+
         $("ul").animate({"left" : leftIndent}, function(){  
             $("li:last").after($("li:first"));   
-            $("ul").css({"left" : "0px"});
+            $("ul").css({"left" : "-31.2vw"});
         });
     });  
 
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
         $("ul").animate({"left" : leftIndent}, function(){
 	        $("li:first").before($("li:last"));
-	        $("ul").css({"left" : "0px"});
+	        $("ul").css({"left" : "-31.2vw"});
         });    
     });  
 });  
