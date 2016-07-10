9dc39bf43aea67cd5c4364a3044503aba0ce0ad2
theking9985 <theking9985@yahoo.com>
2016-07-10 14:40:51 -0700

    in progress match dot with image

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
 
 	// $("li:first").before($("li:last"));
 
-    function matchImageDot(img, dot) {
+    function matchImageWithDot(img, dot) {
     	if ($("li:nth-child(3) img").attr("src") == img) {
     		$(dot).addClass("active").siblings().removeClass("active");
     	}
-    }	
+    };   
 
     $("#right_arrow").click(function(){
         var leftIndent = parseInt($("ul").css("left")) - $("li").outerWidth(true);
@@ -15,16 +15,11 @@ $(document).ready(function() {
             $("li:last").after($("li:first"));   
             $("ul").css({"left" : "-30vw"});
             
-            matchImageDot("4.jpeg", "#dot2");
-            matchImageDot("5.jpeg", "#dot3");
-            matchImageDot("1.jpeg", "#dot4");
-            matchImageDot("2.jpeg", "#dot5");
-            matchImageDot("3.jpeg", "#dot1");
-
-
-            // if ($("li:nth-child(3) img").attr("src") == "4.jpeg") {
-            // 	$("#dot2").addClass("active").siblings().removeClass("active");
-            // }
+            matchImageWithDot("1.jpeg", "#dot4");
+            matchImageWithDot("2.jpeg", "#dot5");
+            matchImageWithDot("3.jpeg", "#dot1");
+            matchImageWithDot("4.jpeg", "#dot2");
+            matchImageWithDot("5.jpeg", "#dot3");
         });
     });  
 
@@ -35,25 +30,17 @@ $(document).ready(function() {
 	        $("li:first").before($("li:last"));
 	        $("ul").css({"left" : "-30vw"});
             
-            matchImageDot("4.jpeg", "#dot2");
-            matchImageDot("5.jpeg", "#dot3");
-            matchImageDot("1.jpeg", "#dot4");
-            matchImageDot("2.jpeg", "#dot5");
-            matchImageDot("3.jpeg", "#dot1");
-        });    
-
-
+            matchImageWithDot("1.jpeg", "#dot4");
+            matchImageWithDot("2.jpeg", "#dot5");
+            matchImageWithDot("3.jpeg", "#dot1");
+            matchImageWithDot("4.jpeg", "#dot2");
+            matchImageWithDot("5.jpeg", "#dot3");
+        });
     });
 
-    // not working dots
-    // $("#dotDiv a").click(function() {
-    // 	$(".active").removeClass("active");
-    // 	$(this).addClass("active");
-    // });
 
 
     	// carousel keeps sliding infinite
-
   //   function checkSize() {
   //   	if ($("h1").css("font-size") == "40px") {
   //   		console.log("checkSize running");
@@ -78,4 +65,29 @@ $(document).ready(function() {
   //   };
 
   //   $(window).resize(checkSize);
+
+  	// in progress matching dot with image
+    // function matchDotWithImage(img, dot) {
+    // 	console.log("here");
+    // 	console.log($(this));
+    // 	if ($(this).attr("id") == dot) {
+    // 		console.log("there");
+	   //      var leftIndent = parseInt($("ul").css("left")) - $("li").outerWidth(true);
+
+	   //      $("ul").animate({"left" : leftIndent}, function(){  
+	   //          $("li:last").after($("li:first"));   
+	   //          $("ul").css({"left" : "-30vw"});
+	   //      });    		
+    // 	}
+    // };
+
+    // $("#dotDiv a").click(function() {
+    // 	$(".active").removeClass("active");
+    // 	$(this).addClass("active");
+    // 	matchDotWithImage($(this).attr("id"), "1.jpeg");
+    // 	matchDotWithImage($(this).attr("id"), "2.jpeg");
+    // 	matchDotWithImage($(this).attr("id"), "3.jpeg");
+    // 	matchDotWithImage($(this).attr("id"), "4.jpeg");
+    // 	matchDotWithImage($(this).attr("id"), "5.jpeg");
+    // });   
 });  
