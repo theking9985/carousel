7a6860b7bc5589e2ed0f1acb66ae3018dfc4cd5e
theking9985 <theking9985@yahoo.com>
2016-07-10 13:18:50 -0700

    solution for different media query

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 
 	// $("li:first").before($("li:last"));
+    
+
 
     $("#right_arrow").click(function(){
-        var leftIndent = parseInt($("ul").css("left")) - $("li").width();
+        var leftIndent = parseInt($("ul").css("left")) - $("li").outerWidth(true);
 
         $("ul").animate({"left" : leftIndent}, function(){  
             $("li:last").after($("li:first"));   
@@ -12,11 +14,39 @@ $(document).ready(function() {
     });  
 
     $("#left_arrow").click(function(){  
-        var leftIndent = parseInt($("ul").css("left")) + $("li").width();
+        var leftIndent = parseInt($("ul").css("left")) + $("li").outerWidth(true);
 
         $("ul").animate({"left" : leftIndent}, function(){
 	        $("li:first").before($("li:last"));
 	        $("ul").css({"left" : "-30vw"});
         });    
-    });  
+    });
+
+
+    	// carousel keeps sliding infinite
+
+  //   function checkSize() {
+  //   	if ($("h1").css("font-size") == "40px") {
+  //   		console.log("checkSize running");
+		//     $("#right_arrow").click(function(){
+		//         var leftIndent = parseInt($("ul").css("left")) - $("li").outerWidth(true);
+
+		//         $("ul").animate({"left" : leftIndent}, function(){  
+		//             $("li:last").after($("li:first"));   
+		//             $("ul").css({"left" : "-40vw"});
+		//         });
+		//     });  
+
+		//     $("#left_arrow").click(function(){  
+		//         var leftIndent = parseInt($("ul").css("left")) + $("li").outerWidth(true);
+
+		//         $("ul").animate({"left" : leftIndent}, function(){
+		// 	        $("li:first").before($("li:last"));
+		// 	        $("ul").css({"left" : "-40vw"});
+		//         });    
+		//     });
+		// }
+  //   };
+
+  //   $(window).resize(checkSize);
 });  
