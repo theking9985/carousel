2569e940a8084b1750b897af0e9a92d942b21b45
theking9985 <theking9985@yahoo.com>
2016-07-10 14:15:54 -0700

    match arrows with dots

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 
 	// $("li:first").before($("li:last"));
-    
 
+    function matchImageDot(img, dot) {
+    	if ($("li:nth-child(3) img").attr("src") == img) {
+    		$(dot).addClass("active").siblings().removeClass("active");
+    	}
+    }	
 
     $("#right_arrow").click(function(){
         var leftIndent = parseInt($("ul").css("left")) - $("li").outerWidth(true);
@@ -10,6 +14,17 @@ $(document).ready(function() {
         $("ul").animate({"left" : leftIndent}, function(){  
             $("li:last").after($("li:first"));   
             $("ul").css({"left" : "-30vw"});
+            
+            matchImageDot("4.jpeg", "#dot2");
+            matchImageDot("5.jpeg", "#dot3");
+            matchImageDot("1.jpeg", "#dot4");
+            matchImageDot("2.jpeg", "#dot5");
+            matchImageDot("3.jpeg", "#dot1");
+
+
+            // if ($("li:nth-child(3) img").attr("src") == "4.jpeg") {
+            // 	$("#dot2").addClass("active").siblings().removeClass("active");
+            // }
         });
     });  
 
@@ -19,13 +34,22 @@ $(document).ready(function() {
         $("ul").animate({"left" : leftIndent}, function(){
 	        $("li:first").before($("li:last"));
 	        $("ul").css({"left" : "-30vw"});
+            
+            matchImageDot("4.jpeg", "#dot2");
+            matchImageDot("5.jpeg", "#dot3");
+            matchImageDot("1.jpeg", "#dot4");
+            matchImageDot("2.jpeg", "#dot5");
+            matchImageDot("3.jpeg", "#dot1");
         });    
+
+
     });
 
-    $("a .dots").click(function() {
-    	$(".active").removeClass("active");
-    	$(this).addClass("active");
-    });
+    // not working dots
+    // $("#dotDiv a").click(function() {
+    // 	$(".active").removeClass("active");
+    // 	$(this).addClass("active");
+    // });
 
 
     	// carousel keeps sliding infinite
